5950f17eef5b48e8b3c266e45079639be0d5c995
texnixwb <133488622+texnixwb@users.noreply.github.com>
2023-07-13 18:31:36 +0300

    Update admin.sql

--- Greenplum/admin.sql
+++ Greenplum/admin.sql
@@ -1,3 +1,18 @@
 SELECT * FROM gp_toolkit.gp_resgroup_config;
 SELECT * FROM gp_toolkit.gp_resgroup_status;
 SELECT * FROM gp_toolkit.gp_resgroup_status_per_host;
+
+--юзер рв и группа юзеров
+create user test_rw with password 'test_rwtest_rw' connection limit 4;
+grant rw to test_rw;
+
+with cte as (select distinct schemaname
+             from pg_stat_user_tables)
+select 'grant usage on schema '||schemaname||' to rw;'
+,'grant select,insert on all tables in schema '||schemaname||' to rw;'
+,'grant select on all sequences in schema '||schemaname||' to rw;'
+from cte
+;
+
+create schema test_;
+alter schema test_ owner to test_rw;
