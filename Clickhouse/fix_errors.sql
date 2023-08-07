c886be84d2383511948c04c6cbca06d01951f195
texnixwb <133488622+texnixwb@users.noreply.github.com>
2023-08-07 16:40:22 +0300

    Update fix_errors.sql

--- Clickhouse/fix_errors.sql
+++ Clickhouse/fix_errors.sql
@@ -1,7 +1,6 @@
     --вместо
 EXCHANGE TABLES buffer.open_registries AND datamart.open_registries; --(не поддерживается на некторых OS)
 --написать:
-RENAME TABLE buffer.open_registries TO buffer.open_registries_tmp
-    , datamart.open_registries TO datamart.open_registries_old
-    , buffer.open_registries_tmp TO datamart.open_registries;
-drop table datamart.open_registries_old;
+RENAME TABLE buffer.open_registries TO buffer.open_registries_tmp 
+, datamart.open_registries TO buffer.open_registries 
+, buffer.open_registries_tmp TO datamart.open_registries;
