ca35620a6c15522f220096a748f76beaa75c609f
texnixwb <133488622+texnixwb@users.noreply.github.com>
2023-11-03 10:14:00 +0300

    Update snappet.sql

--- Clickhouse/snappet.sql
+++ Clickhouse/snappet.sql
@@ -1,2 +1,12 @@
 --преобразование текста в число:
 toInt64(halfMD5(srid)) as rid
+
+-- множественный case
+                                       multiIf(wh_tare_entry = 'ERP', 1,
+                                                wh_tare_entry = 'LOGISTICS', 2,
+                                                wh_tare_entry = 'WBGO', 3,
+                                                wh_tare_entry = 'WH_RPK', 4,
+                                                wh_tare_entry = 'PVZ', 5,
+                                                wh_tare_entry = 'PPVZ',6,
+                                                wh_tare_entry = 'WH_TRBX', 7,
+                                                wh_tare_entry = 'WBBOX', 8, 99) as wh_tare_entry_id,
