6475cac70d591efdc4eacf3326e1ff7265adeb01
texnixwb <133488622+texnixwb@users.noreply.github.com>
2024-01-22 09:35:26 +0300

    Update partitions.sql

--- Clickhouse/partitions.sql
+++ Clickhouse/partitions.sql
@@ -10,30 +10,28 @@ with
     parts as
     (select distinct partition,database,table from system.parts where table='srid_tracker_prepared_v1' and database='srid_tracker')
     ,not_klear as (
-select case when length(part_key)>0 then part_key||','||sorting_key else sorting_key  end as keys
+select replaceAll(case when length(part_key)>0 then part_key||','||sorting_key else sorting_key  end,' ','') as keys
     from( select
       replaceAll(replaceAll(
       replaceAll(replaceAll(replaceAll(replaceAll(partition_key,'toYYYYMMDD',''),'toYYYYMM',''),'toYear',''),'toDate','')
       ,')',''),'(','') as part_key
         , sorting_key
 from system.tables where database=(select distinct database from parts)
-                         and table=(select distinct table from parts)
-         )
-)
-select sql_cript,dd.data_compressed_bytes
+                         and table=(select distinct table from parts)         ))
+select sql_cript,sort_id
     from (
-    select data_compressed_bytes, 'ALTER TABLE '||cc.database||'.'||cc.table||' CLEAR COLUMN '||cc.name||' IN PARTITION '||pp.partition||';' as sql_cript
+    select toInt64(data_compressed_bytes) as sort_id, 'ALTER TABLE '||cc.database||'.'||cc.table||' CLEAR COLUMN '||cc.name||' IN PARTITION '||pp.partition||';' as sql_cript
 from system.columns cc
 join parts pp using (database,table)
     where cc.name not in (select toString(arrayJoin(splitByChar(',', assumeNotNull((select keys from not_klear))) AS src)))
     order by data_compressed_bytes desc
 --теперь дропаем партиции по одной (ибо вся таблица с данными партиций и ордербаем обычно больше 50гб)
 union all
-select 1+row_number() over (partition by 1) as data_compressed_bytes,'ALTER TABLE '||cc.database||'.'||cc.table||' DROP PARTITION '||cc.partition||';' as sql_cript
+select -10-row_number() over (partition by 1) as sort_id,'ALTER TABLE '||cc.database||'.'||cc.table||' DROP PARTITION '||cc.partition||';' as sql_cript
     from parts cc
 --и дроп самой таблицы
 union all
-select 0 as data_compressed_bytes,'drop table '||cc.database||'.'||cc.table||';' as sql_cript
+select -1000 as sort_id,'drop table '||cc.database||'.'||cc.table||';' as sql_cript
     from parts cc group by cc.database,cc.table
 ) as dd
-order by dd.data_compressed_bytes desc;
+order by dd.sort_id desc;
