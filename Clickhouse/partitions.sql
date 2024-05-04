cd225c8b83b0f07c95204df91f62864cc42bedd3
texnixwb <133488622+texnixwb@users.noreply.github.com>
2024-05-04 16:08:35 +0300

    Update partitions.sql

--- Clickhouse/partitions.sql
+++ Clickhouse/partitions.sql
@@ -6,9 +6,10 @@ ALTER TABLE t1 MODIFY SETTINGS 'replication_alter_partitions_sync' = 1;
 --генерация запросов для корректной очистки таблиц перед дропом:
 --очищаем столбцы в партициях от данных, не включая ключ и партиции
 
+--делаем это по партициям, начиная с самой старой
 with
     parts as
-    (select distinct partition,database,table from system.parts where table='srid_tracker_prepared_v1' and database='srid_tracker')
+    (select distinct partition,database,table from system.parts where table='position_changes' and database='positions')
     ,not_klear as (
 select replaceAll(case when length(part_key)>0 then part_key||','||sorting_key else sorting_key  end,' ','') as keys
     from( select
@@ -18,9 +19,11 @@ select replaceAll(case when length(part_key)>0 then part_key||','||sorting_key e
         , sorting_key
 from system.tables where database=(select distinct database from parts)
                          and table=(select distinct table from parts)         ))
-select sql_cript,sort_id
+select sql_cript,sort_id,partition
     from (
-    select toInt64(data_compressed_bytes) as sort_id, 'ALTER TABLE '||cc.database||'.'||cc.table||' CLEAR COLUMN '||cc.name||' IN PARTITION '||pp.partition||';' as sql_cript
+    select toInt64(pp.partition||toString(round(toInt64(data_compressed_bytes)/10000))) as sort_id
+         , 'ALTER TABLE '||cc.database||'.'||cc.table||' CLEAR COLUMN '||cc.name||' IN PARTITION '||pp.partition||';' as sql_cript
+    ,pp.partition
 from system.columns cc
 join parts pp using (database,table)
     where cc.name not in (select toString(arrayJoin(splitByChar(',', assumeNotNull((select keys from not_klear))) AS src)))
@@ -28,10 +31,12 @@ join parts pp using (database,table)
 --теперь дропаем партиции по одной (ибо вся таблица с данными партиций и ордербаем обычно больше 50гб)
 union all
 select -10-row_number() over (partition by 1) as sort_id,'ALTER TABLE '||cc.database||'.'||cc.table||' DROP PARTITION '||cc.partition||';' as sql_cript
+    ,cc.partition
     from parts cc
 --и дроп самой таблицы
 union all
 select -1000 as sort_id,'drop table '||cc.database||'.'||cc.table||';' as sql_cript
+    ,'0' as partition
     from parts cc group by cc.database,cc.table
 ) as dd
-order by dd.sort_id desc;
+order by partition,dd.sort_id desc;
