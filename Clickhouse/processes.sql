ee95f7b356b192e9f38c89306d78b9bd6856b8ca
texnixwb <133488622+texnixwb@users.noreply.github.com>
2024-05-18 10:29:01 +0300

    Update processes.sql

--- Clickhouse/processes.sql
+++ Clickhouse/processes.sql
@@ -2,6 +2,20 @@
 SELECT user    ,round(read_rows / 1000 /1000) as read_rows_kk    ,round(total_rows_approx / 1000 /1000) as total_rows_kk    , round(read_rows / (total_rows_approx / 100), 2) percent
 , elapsed, round((elapsed / percent * 100) - elapsed, 2) left_time, round(read_bytes / 1024 /1024) read_Mgb, round(peak_memory_usage / 1024 /1024) MemoryMb  ,query,query_id 
 FROM system.processes;
+
+--подписанные:
+SELECT user
+     ,toString(round(read_rows / 1000 /1000))||'kk' as read_rows_kk
+     ,toString(round(total_rows_approx / 1000 /1000))||'kk' as total_rows_kk
+     , round(read_rows / (total_rows_approx / 100), 2) as percent
+     , toString(round(elapsed))||'s' as elapsed_s
+     , toString(round((elapsed / percent * 100) - elapsed))||'s' as  left_time
+     , toString(round(read_bytes / 1024 /1024))||'Mb' as read_Mgb
+     , toString(round(peak_memory_usage / 1024 /1024))||'Mb mem' as MemoryMb
+     ,query,query_id
+FROM system.processes;
+
+
 --убить конкретный процесс
 KILL QUERY WHERE query_id='c5b75dbd-fb0d-4802-9d25-06bdfc92201d';
 --убить процессы одного юзера, с ожиданием завершения убийства
