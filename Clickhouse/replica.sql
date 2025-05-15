2471f024906594b013ce9bd9af4ac5213c66f04d
texnixwb <133488622+texnixwb@users.noreply.github.com>
2025-05-15 15:41:23 +0300

    Create replica.sql

---
+++ Clickhouse/replica.sql
@@ -0,0 +1,9 @@
+--Можно для всех таблиц использовать: ENGINE = ReplicatedMergeTree('/clickhouse/tables/{shard}/{database}/{table}', '{replica}')
+--Тогда будет подставляться имя текущее таблицы в зукипер, проверить можно:
+ SELECT
+    database,
+    table,
+    replica_name,
+    zookeeper_path
+FROM system.replicas
+WHERE database = 'datamart' AND table = 'crossborder_srids_and_actions_ts';
