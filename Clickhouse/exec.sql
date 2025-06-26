183358d89ffedbcbfcdd454647a778d5f1cdd691
texnixwb <133488622+texnixwb@users.noreply.github.com>
2025-06-26 15:40:34 +0300

    Update exec.sql

--- Clickhouse/exec.sql
+++ Clickhouse/exec.sql
@@ -2,6 +2,8 @@ set max_memory_usage = 60000000000; -- увеличение использова
 SETTINGS max_memory_usage = '160Gi'; -- как сеттингс при запросе
 -- инсерт в много потоков, стоит использовать, если нет группировки
 SETTINGS max_insert_threads=20
+
+SETTINGS max_execution_time = 30,           -- 30 секунд максимум
 --удаление больших таблиц
 set max_table_size_to_drop='100Ti';
 set max_partition_size_to_drop='100Ti';
