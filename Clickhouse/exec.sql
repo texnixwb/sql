bd54c1a1c6126b9c6d638241de0bc5d8406c940b
texnixwb <133488622+texnixwb@users.noreply.github.com>
2025-08-28 18:08:55 +0300

    Update exec.sql

--- Clickhouse/exec.sql
+++ Clickhouse/exec.sql
@@ -18,3 +18,6 @@ set max_partition_size_to_drop='100Ti';
 
 --группировки argMax потребляющие много памяти:
 settings max_memory_usage = '200Gi',max_bytes_before_external_group_by = '160Gi'; -- при достижении памяти 164гб будет свапить на диск, работает достаточно быстро и без переполнения.
+
+--чтобы меньше потребляло памяти, если таблица/таблицы все группируются по ордербаю
+optimize_aggregation_in_order = 1
