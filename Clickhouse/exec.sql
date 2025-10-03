fcfe2d7e1f9827e9f668860322adc123a67ade5c
texnixwb <133488622+texnixwb@users.noreply.github.com>
2025-10-03 16:17:24 +0300

    Update exec.sql

--- Clickhouse/exec.sql
+++ Clickhouse/exec.sql
@@ -12,8 +12,8 @@ SETTINGS
     max_execution_time = 30,           -- 30 секунд максимум
     timeout_before_checking_execution_speed = 10;  -- Проверка скорости через 10 сек
 --удаление больших таблиц
-set max_table_size_to_drop='100Ti';
-set max_partition_size_to_drop='100Ti';
+set max_table_size_to_drop=0;
+set max_partition_size_to_drop=0;
 
 
 --группировки argMax потребляющие много памяти:
